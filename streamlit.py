d81f7c6721f5d441abd195b3a1d5dcb06afc222a
NANDHIRAJA K <126322649+nandhiraja@users.noreply.github.com>
2024-06-05 19:28:28 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
+
 st.write("""
 # Simple Iris Flower Prediction App
                    by Nandhiraja
